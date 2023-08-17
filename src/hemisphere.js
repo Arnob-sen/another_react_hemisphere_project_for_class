3ac001e2d37873661be4c026fbcc004f1f6b1053
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-17 12:10:07 +0600

    finding_geolocation by functional component

--- src/hemisphere.js
+++ src/hemisphere.js
@@ -1,6 +1,7 @@
 import React from "react";
 const hemisphere=()=>
 {
+   
     return (
         <div>
             you are from northern hemisphere
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 
 const App=()=>
 {
+    window.navigator.geolocation.getCurrentPosition(
+        (position)=>console.log(position),
+       (error)=> console.log(error)
+    );
     return(
         <div>
             hello world
