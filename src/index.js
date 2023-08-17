c768472e67d561ea3e0b44f297cb581f443cee01
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-17 13:28:29 +0600

    create_class_base_component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
-const App=()=>
-{
-    window.navigator.geolocation.getCurrentPosition(
-        (position)=>console.log(position),
-       (error)=> console.log(error)
-    );
-    return(
-        <div>
-            hello world
-        </div>
-    )
+
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>console.log(position),
+           (error)=> console.log(error)
+        )
+        
+        return(
+            <div>
+                hello world
+            </div>
+        )
+    }
+
+    
 }
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
