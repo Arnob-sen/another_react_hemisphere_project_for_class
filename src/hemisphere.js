c12f1021e21fd10a22a98d6a1f548336fe019cb3
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-17 10:38:46 +0600

    created_hemisphere_file

---
+++ src/hemisphere.js
@@ -0,0 +1,10 @@
+import React from "react";
+const hemisphere=()=>
+{
+    return (
+        <div>
+            you are from northern hemisphere
+        </div>
+    )
+}
+export default hemisphere;
