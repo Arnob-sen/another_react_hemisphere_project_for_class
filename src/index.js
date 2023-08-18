854350e033a83c3bdc9264341d1175e9aa7c275e
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-18 10:40:56 +0600

    creating_error_handeling

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ class App extends React.Component{
     constructor(props)
     {
         super(props)// here we override props from parent class react.component.so props of parent class should work perfectly.thats why super 
-        this.state={latidue:null}//initialize state
+        this.state={latidue:null,errorMessage:''}//initialize state
         window.navigator.geolocation.getCurrentPosition(
             (position)=>{
-                this.setState({latidue:position.coords.latitude})//this for update state.any time to change,update,manipulate use setState method
+                this.setState({latidue:position.coords.latitude})//this for update state.any time to change,update,manipulate use setState method.this will use 2nd time.1st time render show the initial value.then go the update method.then rerender it and show the updated value
                 // this.state.latidue=position.coords.latitude//this is the direct assignment.never do this.this is only use when initialize state
             },
-           (error)=> console.log(error)
+           (error)=>{
+            this.setState({errorMessage:error.message})
+           }
         )
     }
     render(){
@@ -21,6 +23,7 @@ class App extends React.Component{
         return(
             <div>
                 my latidue is {this.state.latidue}
+                and error message is  {this.state.errorMessage}
             </div>
         )
     }
