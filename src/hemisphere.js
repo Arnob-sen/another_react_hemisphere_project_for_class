f7d97d7da9b85b38e56e2f67b12460bfb2781653
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-18 17:32:54 +0600

    add_some_style

--- src/hemisphere.js
+++ src/hemisphere.js
@@ -1,16 +1,30 @@
 import React from "react";
 import northernpic from './image/northern.jpg';
 import southernpic from './image/southern.webp';
+import './hemisphere.css';
+const HemisphereConf={
+    Northern:{
+        text:'its northern hemisphere',
+        picture:northernpic,
+
+    },
+    Southern:{
+        text:"Its sourthern hemisphere",
+        picture:southernpic,
+    }
+}
 const Hemisphere=({latidue})=>
 {
-    const check=latidue>0 ?'Northern':'southern';
-    const pic=latidue>0?northernpic:southernpic
+    const check=latidue>0 ?'Northern':'Southern';
+    const { text,picture } =HemisphereConf[check];
+    
    
     return (
-        <div>
+        <div className={check}>
           
-            <img src={pic} alt={check} />
-            you are from {check} hemisphere
+            <img src={picture} alt={check} />
+            {text}
+            
         </div>
     )
 }
