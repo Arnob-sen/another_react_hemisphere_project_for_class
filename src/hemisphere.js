5fcf05d2813f676f651bf3962be01aa1ead47b95
Arnob-sen <arnobsen16.2@gmail.com>
2023-08-18 16:19:44 +0600

    add_picture_and_get the image

--- src/hemisphere.js
+++ src/hemisphere.js
@@ -1,10 +1,15 @@
 import React from "react";
+import northernpic from './image/northern.jpg';
+import southernpic from './image/southern.webp';
 const Hemisphere=({latidue})=>
 {
     const check=latidue>0 ?'Northern':'southern';
+    const pic=latidue>0?northernpic:southernpic
    
     return (
         <div>
+          
+            <img src={pic} alt={check} />
             you are from {check} hemisphere
         </div>
     )
